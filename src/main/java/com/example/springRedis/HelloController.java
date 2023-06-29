491e146cd7b3f0bfa65e4242ab7216de1ff290de
melody-story <rnlqls7454@naver.com>
2023-06-29 23:17:32 +0900

    Redis 연동

--- src/main/java/com/example/springRedis/HelloController.java
+++ src/main/java/com/example/springRedis/HelloController.java
@@ -1,13 +1,35 @@
 package com.example.springRedis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    // 스프링에서제공하는 추상화된 redis template - String 을 가지고 작업
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
     @GetMapping("/helloRedis")
     public String HelloRedis() {
         return "Hello Redis!!";
+    }
+    
+    @GetMapping("/setFruit")
+    public String setFruit(@RequestParam String name) {
+        ValueOperations<String, String> ops =  redisTemplate.opsForValue();
+        ops.set("fruit", name);
+        return "Saved,";
+    }
+    
+    @GetMapping("/getFruit")
+    public String getFruit() {
+        ValueOperations<String, String> ops =  redisTemplate.opsForValue();
+        String fruitName = ops.get("fruit");
+        return fruitName;
     }
 }
